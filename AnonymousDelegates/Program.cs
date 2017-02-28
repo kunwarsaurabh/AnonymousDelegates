68a944d30ce7cca860be06c7a60005963ce6b8d1
Kunwar Saurabh <saurabhsrivastava@live.in>
2017-02-28 08:58:10 +0530

    FirstCommit

--- AnonymousDelegates/Program.cs
+++ AnonymousDelegates/Program.cs
@@ -8,8 +8,17 @@ namespace AnonymousDelegates
 {
     class Program
     {
+        delegate int PointToAddFunc(int num1, int num2);
         static void Main(string[] args)
         {
+            PointToAddFunc padd = Program.Add;
+            Console.WriteLine(padd.Invoke(2,4));
+            Console.ReadLine();
+        }
+
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
